3560e05fb0face82f52b7e8ce5830f8a9422815f
Geetha Sri <geethu6997@gmail.com>
2025-08-10 10:53:48 +0530

    Remove duplicate app.js and fix start script

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 require("reflect-metadata");
 const express = require("express");
 const morgan = require("morgan"); 
-const { AppDataSource } = require("./data-source");
+const { AppDataSource } = require("./src/data-source");
+
 
 const airportRoutes = require("./routes/airport");
 const app = express();
